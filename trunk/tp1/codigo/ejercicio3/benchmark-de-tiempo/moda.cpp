bab94ca0f54820d40c7c3b85a0b543d1ab67a972
seges.ar <seges.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-04-02 21:23:58 +0000

    sube en el ejercicio3, el benchmark de tiempo con los graficos.

---
+++ trunk/tp1/codigo/ejercicio3/benchmark-de-tiempo/moda.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include <assert.h>
+#include "hr_time.h"
+
+using namespace std;
+
+int moda(int* a, int n) {
+    int indiceDeAtras = 0;
+    int indiceDeAdelante = 0;
+
+    //en tachados guardaremos que elementos descartamos porq encontramos uno distinto
+    bool* tachados = new bool[n];
+    for (int i = 0; i < n; i++) {
+        tachados[i] = false;
+    }
+
+    while (indiceDeAdelante < n) {
+        // si son distintos pasan a estar tachados
+        if (a[indiceDeAtras] != a[indiceDeAdelante]) {
+            tachados[indiceDeAtras] = true;
+            tachados[indiceDeAdelante] = true;
+
+            // movemos los indices para que lleguen al proximo elemento sin tachar, y ademas que valga
+            // que el de adelante quede adelante
+            while (indiceDeAtras < n && tachados[indiceDeAtras]) {
+                indiceDeAtras++;
+            }
+            while (indiceDeAdelante < n && (tachados[indiceDeAdelante] || indiceDeAdelante <= indiceDeAtras)) {
+                indiceDeAdelante++;
+            }
+        }
+
+        // si eran iguales, avanzamos buscando otro diferente
+        else {
+            indiceDeAdelante++;
+        }
+    }
+
+
+    // buscamos el primer elemento sin tachar y sabemos que es la moda
+    // o quedo solo 1 y es la moda, o quedaron varios iguales y que son la moda
+
+
+    /*int k = 0;
+    while ( k < n && tachados[k]) {
+        k++;
+    }*/
+    delete [] tachados;
+    assert(indiceDeAtras < n);
+    return a[indiceDeAtras];
+}
+
+int main(int argc, char* argv[]) {
+
+    // leo los datos de entrada
+    string ruta;
+    if(argc >= 2) {
+        ruta = argv[1];
+    } else {
+        ruta="Tp1Ej1.in";
+    }
+    fstream f (ruta.c_str());
+    assert(f.is_open());
+
+    // preparo el archivo de salida
+    string salida;
+    if(argc > 2) {
+        salida = argv[2];
+    } else {
+        salida = "Tp1Ej1.out";
+    }
+    ofstream o (salida.c_str());
+
+    int buffer, elementos;
+    f >> buffer;
+    while (buffer != 0) {
+        elementos = buffer;
+        int* a = new int[elementos];
+        int i = 0;
+        while (elementos > 0) {
+            f >> a[i];
+            i++;
+            elementos--;
+        }
+
+        //arranco el timer
+        CStopWatch tiempo;
+        tiempo.startTimer();
+
+
+        int res = moda(a,buffer);
+
+        //paro el timer y calculo el tiempo trancurrido
+        tiempo.stopTimer();
+        double tiempoTotal = tiempo.getElapsedTime();
+        //cout << res;
+        //"tiempo total :"
+        cout << tiempoTotal << endl;
+        delete [] a;
+        o << res << endl;
+        f >> buffer;
+    }
+    return 0;
+}
+
