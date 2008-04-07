a4ecb7dd0df87f43bba13f852bb03bfa3f9908a4
federicoemartinez <federicoemartinez@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-04-07 01:07:36 +0000

    demo del 3

--- trunk/tp1/codigo/ejercicio2/benchmark/generador.py
+++ trunk/tp1/codigo/ejercicio2/benchmark/generador.py
@@ -1,12 +1,12 @@
 import random
 
-for n in range(1,100):
+for n in range(1,36):
 	print "Caso"
 	print n," ",
 	print 200
 	x=[[0,0] for i in range(n)]
 	for i in range(n):
-		x[i][0] = 1
-		x[i][1] = 1
+		x[i][0] = random.randint(1,100)
+		x[i][1] = random.randint(1,100)
 		print x[i][0], " ", x[i][1]
 print "Fin"
