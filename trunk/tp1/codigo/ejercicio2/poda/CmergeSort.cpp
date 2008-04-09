ea92a5b4e36d4786378a78714229f76008e9f089
HadesKleizer <HadesKleizer@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-04-09 05:46:04 +0000

    mergesort es realmente mergesort

--- trunk/tp1/codigo/ejercicio2/poda/CmergeSort.cpp
+++ trunk/tp1/codigo/ejercicio2/poda/CmergeSort.cpp
@@ -3,80 +3,64 @@
 CMergeSort::CMergeSort(Cosa* cos, int cantEle, int* inds){
     opers = opers + 3;
 	cosas = cos;
-	cantElem=cantEle;
-	indices = inds;
-}
-
-unsigned long long int
-CMergeSort::sort() {
-    opers = 0;
+	cantElem = cantEle;
+    for(int i = 0; i < cantEle; i++){
+        inds[i] = i;
+    }
+    indices = inds;
     mergeSort(0,cantElem-1);
-    return opers;
 }
 
-void 
-CMergeSort::mergeSort(int pri, int ult) {
-    opers++;
-    if(pri<ult) {
-        opers = opers + 3;
-        int pmedio=(pri + ult)/2;
+void CMergeSort::mergeSort(int pri, int ult) {
+    if(pri < ult) {
+        int pmedio = (pri + ult)/2;
 
         mergeSort(pri, pmedio);
-        opers++;
         mergeSort(pmedio+1, ult);
         merge(pri,ult);
     }
 }
 
-void 
-CMergeSort::merge(int pri, int ult) {
-    opers = opers + 7;
-    int i,j, cont=0;
-    int tamano=ult-pri+1;
-    int pmedio=(pri+ult)/2;
+void CMergeSort::merge(int pri, int ult) {
+    int i,j, cont = 0;
+    int tamano = ult - pri + 1;
+    int pmedio = (pri + ult)/2;
 
-    opers = opers + tamano + tamano;
     Cosa *tmp=new Cosa[tamano];
     int * indicesAux = new int[tamano];
-    opers = opers + 4;  
-    for(i=pri;i<=pmedio;i++,cont++)
+    for(i = pri; i <= pmedio; i++)
     {
-        opers = opers + 5;
-        tmp[cont]=cosas[i];
+        tmp[cont] = cosas[i];
         indicesAux[cont] = indices[i];
+        cont++;
     }
 
-    opers = opers + 5;
-    for(i=ult;i>=pmedio+1;i--,cont++)
+    for(i = ult; i > pmedio; i--)
     {
-        opers = opers + 6;
-        tmp[cont]=cosas[i];
+        tmp[cont] = cosas[i];
     	indicesAux[cont] = indices[i];
+        cont++;
     }
 
-    opers = opers + 6;
-    for(i=0,j=tamano-1,cont=pri;i<=j;cont++)    
+    cont = pri;
+    for(i = 0, j = tamano - 1; i <= j;)
     {
-        opers++;
-        if(tmp[i].peso<=tmp[j].peso)
+        if(tmp[i].peso <= tmp[j].peso)
         {
-            opers = opers + 3;
             cosas[cont]=tmp[i];
             indices[cont] = indicesAux[i];
             i++;
         }
         else
         {
-            opers = opers + 3;
-            cosas[cont]=tmp[j];
+            cosas[cont] = tmp[j];
             indices[cont] = indicesAux[j];
             j--;
         }
+        cont++;
     }
-    opers++;
     delete[] indicesAux;
     delete[] tmp;
-    
 }
 
 
--- trunk/tp1/codigo/ejercicio2/poda/CmergeSort.h
+++ trunk/tp1/codigo/ejercicio2/poda/CmergeSort.h
@@ -3,17 +3,17 @@
 
 #include <iostream>
 #include "Cosa.h"
-static unsigned long long int opers= 0L; 
+static unsigned long long int opers = 0L; 
+
 class CMergeSort{
-     public:
-            CMergeSort(Cosa* cos, int cantElem,int* inds);
-            unsigned long long int sort();
-    private:
-            Cosa* cosas;
-            int * indices;
-            int cantElem;
-            void mergeSort(int desde, int hasta);
-            void merge(int desde, int hasta);
+public:
+    CMergeSort(Cosa* cos, int cantElem, int* inds);
+private:
+    Cosa* cosas;
+    int * indices;
+    int cantElem;
+    void mergeSort(int desde, int hasta);
+    void merge(int desde, int hasta);
 };
 
 #endif /*_CmergeSort_h*/
