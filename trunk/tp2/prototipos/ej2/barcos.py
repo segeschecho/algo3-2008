aba4b4277324cc19d4abdfc365c3e3cd66fb5104
gomox.ar <gomox.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-04-23 07:26:59 +0000

    Agregados prototipos funcionando del ejercicio 2, mas un generador de instancias y un verificador de soluciones.

---
+++ trunk/tp2/prototipos/ej2/barcos.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+from grafocircular import GrafoCircular
+
+#################################################################
+# Versión de decisión                                           #
+#################################################################
+
+def hayCaminoTerminaEnA(a,b,g):
+    n = g.n
+    if b-a == 1 or (b == 0 and a == n-1):
+        return g.estanConectados(a,b)
+   
+    return (hayCaminoTerminaEnA((a+1) % n, b, g) and g.estanConectados(a, (a+1) % n)) or \
+           (hayCaminoTerminaEnB((a+1) % n, b, g) and g.estanConectados(a,b))
+
+def hayCaminoTerminaEnB(a,b,g):
+    n = g.n
+    if b-a == 1 or (b == 0 and a == n-1):
+        return g.estanConectados(a,b)
+
+    return (hayCaminoTerminaEnA(a, (b-1) % n, g) and g.estanConectados(a,b)) or \
+           (hayCaminoTerminaEnB(a, (b-1) % n, g) and g.estanConectados((b-1) % n,b))
+
+def hayCamino(g):
+    for i in range(g.n):
+        a = i
+        b = (i-1) % g.n
+        if hayCaminoTerminaEnA(a,b,g) or hayCaminoTerminaEnB(a,b,g):
+            return True
+
+        return False
+
+# version "verbose" que tira mas informacion
+def hayCamino2(g):
+    print "Buscando caminos (%s nodos, %s acuerdos)" % (g.n, g.a)
+    for i in range(g.n):
+        a = i
+        b = (i-1) % g.n
+        print "  Camino de %s a %s:" % (a,b)
+        p = 0
+        if hayCaminoTerminaEnA(a,b,g): 
+            print "    Hay un camino que empieza en %s" % a
+            p = 1
+        if hayCaminoTerminaEnB(a,b,g):
+            print "    Hay un camino que empieza en %s" % b
+            p = 1
+        if p != 1:
+            print "    Ninguno"
+
+
+#################################################################
+# Versiones de construcción de caminos (recursión sin tablas)   #
+#################################################################
+
+
+def caminoQueTerminaEnA(a,b,g):
+    # caso base
+    if b-a == 1 or (b == 0 and a == g.n-1):
+        if g.estanConectados(a,b):
+            return [b,a]
+        else:
+            return []
+
+    # caso recursivo
+    c = caminoQueTerminaEnA((a+1) % g.n, b, g)
+    if c != [] and g.estanConectados(a, (a+1) % g.n):
+        return c + [a]
+    c = caminoQueTerminaEnB((a+1) % g.n, b, g)
+    if c != [] and g.estanConectados(a,b):
+        return c + [a]
+
+    return []
+
+def caminoQueTerminaEnB(a,b,g): 
+    # caso base
+    if b-a == 1 or (b == 0 and a == g.n-1):
+        if g.estanConectados(a,b):
+            return [a,b]
+        else:
+            return []
+    
+    # caso recursivo
+    c = caminoQueTerminaEnA(a, (b-1) % g.n, g)
+    if c != [] and g.estanConectados(a,b):
+        return c + [b]
+    c = caminoQueTerminaEnB(a, (b-1) % g.n, g)
+    if c != [] and g.estanConectados((b-1) % g.n, b):
+        return c + [b]
+
+    return []
+
+
+# algoritmo de busqueda
+def generarCamino(g):
+    for i in range(g.n):
+        a = i
+        b = (i-1) % g.n
+        
+        c = caminoQueTerminaEnA(a,b,g)
+        if c != []:
+            return c
+
+        c = caminoQueTerminaEnB(a,b,g)
+        if c != []:
+            return c
+
+    return []
+
+
+#################################################################
+# Prueba simple                                                 #
+#################################################################
+
+
+if __name__ == '__main__':
+    # Ejemplo del enunciado
+    #acuerdos = [(1,2),(2,3),(2,4),(2,5),(3,0),(4,0),(4,5)]
+    #n = 6
+    #g = GrafoCircular(n, acuerdos)
+
+    from barcos_generador import generarInstancia
+    from barcos_tester import chequearCaminoValido
+
+    g = generarInstancia()
+    print g
+    c = generarCamino(g)
+    print c
+    
+    if c is [] and hayCamino(g):
+        print "No se pudo encontrar el camino y sin embargo parecía haberlo!"
+    
+    if c != []:
+        chequearCaminoValido(g, c)
---
+++ trunk/tp2/prototipos/ej2/barcos_tester.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+from grafocircular import GrafoCircular
+
+#################################################################
+# Verificador de que un camino es valido                        #
+#################################################################
+
+class CaminoNoValido(Exception):
+    pass
+
+def noHayRepetidos(l):
+    v = l[:]
+    v.sort()
+    for i in range(len(v)-1):
+        if v[i] == v[i+1]:
+            return False
+    return True
+
+
+# lanza una excepcion medianamente descriptiva si el camino no es valido
+def chequearCaminoValido(grafo, camino):
+    
+    # se pasa por todos?
+    try:
+        assert(len(camino)) == grafo.n
+        assert(noHayRepetidos(camino))
+    except AssertionError:
+        raise CaminoNoValido, "El camino propuesto no pasa por todos los nodos del grafo."
+
+    
+    # se pasa usando un camino valido?
+    for i in range(len(camino)-1):
+        if not grafo.estanConectados(camino[i], camino[i+1]):
+            raise CaminoNoValido, "El camino propuesto une dos ciudades sin acuerdo (%s y %s)." % (i,i+1)
+
+    
+    # el camino no se cruza con si mismo?
+    tachados = [camino[0]]
+    def siguientes(n):    
+        i = (n+1) % grafo.n
+        while i in tachados:
+            i = (i+1) % grafo.n
+        j = (n-1) % grafo.n
+        while j in tachados:
+            j = (j-1) % grafo.n
+        return (i,j)
+
+    for i in range(len(camino)-1):
+        actual = camino[i]
+        next = camino[i+1]
+        if not next in siguientes(actual):
+            raise CaminoNoValido, "El camino se cruza con si mismo."
+        else:
+            tachados.append(next)
+
+
+
+# devuelve bool indicando si el camino es valido o no
+def esCaminoValido(grafo, camino):
+    try:
+        chequearCaminoValido(grafo, camino)
+    except:
+        return False
+
+    return True
+
