87add9d15cf6e244b80626b8f09b97a839dcab41
gomox.ar <gomox.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-04-25 20:11:13 +0000

    Corregido un bug un tanto grosero en el codigo del ejercicio 2 en Python, y terminado de pasar a C++.

--- trunk/tp2/codigo/ejercicio2/BuscadorCaminoTCI.h
+++ trunk/tp2/codigo/ejercicio2/BuscadorCaminoTCI.h
@@ -14,6 +14,7 @@ class BuscadorCaminoTCI {
         BuscadorCaminoTCI(Grafo& grafo);
         ~BuscadorCaminoTCI();
         list<unsigned> buscarCamino();
+        void imprimirCamino();
     private:
         Grafo& g;
         bool resuelto;
