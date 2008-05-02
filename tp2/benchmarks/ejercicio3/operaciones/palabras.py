90ec1904496821329cf0796b79c1a96178e916cf
seges.ar <seges.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-05-02 17:46:36 +0000

    2 test para el parser, con el formato pedido en el enunciado, de 200 y 500 palabras.. en la secuencia agregar - pertenece - sacar

--- tp2/benchmarks/ejercicio3/operaciones/palabras.py
+++ tp2/benchmarks/ejercicio3/operaciones/palabras.py
@@ -1,13 +1,13 @@
 import random
-print 500
+print 200
 
 letras = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-for i in range(501,1001):
+for i in range(501,701):
     x=random.randint(0,len(letras)-1)
     s=""
     for j in range(i):
         s+=str(letras[(x+j) % len(letras)])
-    print s
+    print "*",s
 
         
         
