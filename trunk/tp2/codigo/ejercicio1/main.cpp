b0b59e9af2927ad1f2ee6f8e1d521dfd70eb0238
federicoemartinez <federicoemartinez@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-05-03 03:10:24 +0000

    algunos cambios chiquitos, agregada macro para parametros, agregados mas comentarios al codigo del 1

--- trunk/tp2/codigo/ejercicio1/main.cpp
+++ trunk/tp2/codigo/ejercicio1/main.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <fstream>
 #include <assert.h>
-#define no !
 #define print cout<<
 #include "Grafo.h"
 using namespace std;
@@ -69,22 +68,31 @@ int main(int argc, char* argv[])
         }
 	return 0;
     }
-
+	
 /* dfs que ademas numera los nodos por fin de llamada */
 /* en valor[num] dejamos al nodo con el valor num */
+/*  	parametros:
+*   	grafo a recorrer
+* 	nodo donde estoy parado
+*	numero a poner en el nodo 
+*	marcador de visitados
+*	marcador de valores asignados
+*/  
 void dfs(Grafo* grafo, unsigned nodo, unsigned& num, bool* visitado, unsigned* valor){
     visitado[nodo] = true;
     list<unsigned> :: iterator principio = grafo->verticesOut[nodo].begin();
     list<unsigned> :: iterator final = grafo->verticesOut[nodo].end();
     for(list<unsigned> :: iterator each = principio; each != final; each++){
-        if (no visitado[*each])
+        if (! visitado[*each])
             dfs(grafo,*each,num,visitado,valor);
     }
+	// guardamos a que nodo le toco el valor num
     valor[num] = nodo;
+	// incrementamos num para que el padre de este nodo, tenga numero mas alto
     num += 1;
 }
 
-/* parte recursiva del dfs */
+/*  se encarga de llamar dfs para numerar, de modo de que se visiten todos los nodos */
 void auxdfs(Grafo* grafo, bool* visitado, unsigned* valor){
 
     for(unsigned i = 0; i < grafo->nodos; i++){
@@ -93,7 +101,7 @@ void auxdfs(Grafo* grafo, bool* visitado, unsigned* valor){
     }
     unsigned num = 0;
         for(unsigned i = 0;  i < grafo->nodos; i++){
-            if(no visitado[i]){
+            if(! visitado[i]){
                 dfs(grafo,i,num,visitado,valor);
             }
 	}
@@ -106,7 +114,7 @@ void dfs2(Grafo* grafo, unsigned nodo,bool* visitado, list<unsigned>* fuerte){
     list<unsigned> :: iterator principio = grafo->verticesOut[nodo].begin();
     list<unsigned> :: iterator final = grafo->verticesOut[nodo].end();
     for(list<unsigned> :: iterator each = principio; each != final; each++){
-        if (no visitado[*each])
+        if (! visitado[*each])
             dfs2(grafo,*each,visitado,fuerte);
     }
     fuerte->push_back(nodo);
@@ -116,7 +124,7 @@ void dfs2(Grafo* grafo, unsigned nodo,bool* visitado, list<unsigned>* fuerte){
 Grafo* invertirGrafo(Grafo* grafo){
     list<pair<unsigned, unsigned> > x;
 
-
+	// miramos cada nodo y sus relaciones , y ponemos el par inverso
     for(unsigned i = 0; i < grafo->nodos; i++){
         list<unsigned> :: iterator principio = grafo->verticesOut[i].begin();
         list<unsigned> :: iterator final = grafo->verticesOut[i].end();
@@ -124,12 +132,13 @@ Grafo* invertirGrafo(Grafo* grafo){
             x.push_back(pair<unsigned,unsigned>(*each,i));
         }
     }
+	//creamo el grafo a devolver
     Grafo* g = new Grafo(grafo->nodos,x);
 
     return g;
 }
 
-/* arma las componentes fuertemente conexas segun el algoritmo de kosaraju */
+/* arma las componentes fuertemente conexas segun el algoritmo de Kosaraju */
 list<list<unsigned>* > * armarFuertes(Grafo* grafo,bool*visitado,unsigned* valor){
     // primero numeramos los nodos
     auxdfs(grafo,visitado,valor);
@@ -148,7 +157,7 @@ list<list<unsigned>* > * armarFuertes(Grafo* grafo,bool*visitado,unsigned* valor
     // no recorre todo el arbol, sino solo una componente fuertemente conexa por vez
     for(int i = grafo->nodos-1; i >= 0; i--){
 
-        if (no visitado[valor[i]]){
+        if (! visitado[valor[i]]){
 
             dfs2(g,valor[i],visitado,fuerte);
 
@@ -164,6 +173,7 @@ list<list<unsigned>* > * armarFuertes(Grafo* grafo,bool*visitado,unsigned* valor
 
 /* resuelve el ejercicio 1 */
 bool* ejercicio1(Grafo* grafo,unsigned rels, unsigned & cantGanadores){
+	// con menos de n-1 aristas el grafo subyacente no es conexo, sabemos que no se puede arreglar
     if(rels < grafo->nodos - 1){
         cantGanadores = 0;
         return NULL;
@@ -196,6 +206,7 @@ bool* ejercicio1(Grafo* grafo,unsigned rels, unsigned & cantGanadores){
 	    // filtramos las flechas que van dentro de la componente conexas
 	    // pueden quedar flechas intercomponentes repetidas, pero no afectan al algoritmo,
 	    // ni al orden, ya que a lo sumo hay m flechas
+		// ademas si tomo la flecha a -> b, cuando la vuelva a tomar, ya visite a b y no entro
             if(dondeQuedo[i] != dondeQuedo[*each])
                 x.push_back(pair<unsigned,unsigned>(dondeQuedo[i], dondeQuedo[*each]));
         }
@@ -208,10 +219,12 @@ bool* ejercicio1(Grafo* grafo,unsigned rels, unsigned & cantGanadores){
     // El nuevo grafo no tiene ciclos, entonces podemos afirmar que alguien gana si y solo si hay un unico nodo
     // con grado de entrada = 0
     for(unsigned each = 0 ; each < g1.nodos; each++){
+		// encontramos uno que podria ser ganador
         if(g1.verticesIn[each].empty() && not yaHayUno){
             quien = new unsigned(each);
             yaHayUno = true;
         }
+		// ya teniamos un candidato, entonces no puede haber ganador
         else if(g1.verticesIn[each].empty() && yaHayUno){
         delete quien;
 	    quien = NULL;
@@ -219,26 +232,22 @@ bool* ejercicio1(Grafo* grafo,unsigned rels, unsigned & cantGanadores){
 	    return NULL;
         }
     }
-    //cosa que no debe (ni puede) pasar :P
-    if(no yaHayUno){
-        cout<<"Error inesperado"<<endl;
-    }
     //buscamos a la componente ganadora (si hay)
     list<unsigned> * l = new list<unsigned>();
     if(quien != NULL){
-	int aux = *quien;;
-	list<list<unsigned>* > :: iterator it = fuertes->begin();
-	for(list<list<unsigned>* > :: iterator it = fuertes->begin(); aux > 0; aux --){
-		it++;
-	}
-	l->assign((*it)->begin(),(*it)->end());
-	delete quien;
+		int aux = *quien;;
+		list<list<unsigned>* > :: iterator it = fuertes->begin();
+		for(list<list<unsigned>* > :: iterator it = fuertes->begin(); aux > 0; aux --){
+			it++;
+		}
+		l->assign((*it)->begin(),(*it)->end());
+		delete quien;
     }
 
     for(list<list<unsigned>* > :: iterator each = principio; each != final;){
-	list<list<unsigned>* > :: iterator it = each;
-	each++;
-	delete *it;
+		list<list<unsigned>* > :: iterator it = each;
+		each++;
+		delete *it;
     }
     // como hay que devolverlas en orden hacemos bucketsort en O(n)
     bool* ganadores = new bool[grafo->nodos];
