b418cf0dbe0a74d979e43228e6fd215c2aea453c
gomox.ar <gomox.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-05-29 20:16:21 +0000

    Agregada funcionalidad adicional del grafo bipartito y el dibujo.

--- tp3/prototipos/GrafoBipartito.py
+++ tp3/prototipos/GrafoBipartito.py
@@ -29,18 +29,43 @@ class GrafoBipartito:
             try:
                 assert (a in p1 and b in p2) or (a in p2 and b in p1)
             except AssertionError:
-                raise AssertionError, "Error: el eje %s no cruza de p1 a p2." % (a,b)
+                raise AssertionError, "Error: el eje (%s,%s) no cruza de p1 a p2." % (a,b)
+
 
 
     def estaRel(self, a, b):
         return ((a,b) in self.ejes) or \
                ((b,a) in self.ejes)
 
+    def __eq__(self, otro):
+        return self.ejes == otro.ejes and \
+               self.p1 == otro.p1 and \
+               self.p2 == otro.p2
+
+    def __ne__(self, otro):
+        return not (self == otro)
+
+    def __repr__(self):
+        return "<GrafoBipartito #p1=%s #p2=%s m=%s>" % (len(p1),len(p2),m)
+
+    def __str__(self):
+        buf = []
+        buf.append("------------------------")
+        buf.append("GrafoBipartito")
+        buf.append("------------------------")
+        buf.append("P1 = %s" % list(self.p1))
+        buf.append("P2 = %s" % list(self.p2))
+        buf.append("Ejes = %s" % list(self.ejes))
+        buf.append("------------------------")
+        return '\n'.join(buf)
+
+
 class Dibujo:
     def __init__(self, g, l1, l2):
         self.g = g
         self.l1 = l1
         self.l2 = l2
+        self.cruces = None
 
         # chequeos misc
         # tipos
@@ -48,10 +73,66 @@ class Dibujo:
         
         # dibujo valido
         try:
-            assert Set(l1).issubset(g.p1)
-            assert Set(l2).issubset(g.p2)
+            s1 = Set(l1)
+            s2 = Set(l2)
+            
+            # no hay repetidos
+            assert len(s1) == len(l1)
+            assert len(s2) == len(l2)
+            
+            # estan incluidos en la particion del grafo
+            assert s1.issubset(g.p1)
+            assert s2.issubset(g.p2)
+
         except AssertionError:
             raise AssertionError, "Error: el dibujo propuesto no es valido para este grafo."
+
+    def esIncrementalDe(self,d):
+        if self.g != d.g:
+            return False
+
+        l1c = [x for x in self.l1 if not x in d.l1]
+        if l1c != d.l1:
+            return False
+
+        l2c = [x for x in self.l2 if not x in d.l2]
+        if l2c != d.l2:
+            return False
+        
+        return True
+
+
+    def contarCruces(self):
+        if not self.cruces is None:
+            return self.cruces
+        else:
+            self.cruces = self._contarCruces()
+
+    def _contarCruces(self):
+        raise NotImplementedError
+ 
+    def __eq__(self, otro):
+        return self.l1 == otro.l1 and \
+               self.l2 == otro.l2 and \
+               self.g1 == otro.g1
+
+    def __ne__(self, otro):
+        return not (self == otro)
+
+    def __repr__(self):
+        return "<Dibujo de GrafoBipartito (#l1=%s, #l2=%s)>" % (len(self.l1),len(self.l2))
+
+    def __str__(self):
+        buf = []
+        buf.append("------------------------")
+        buf.append("Dibujo de GrafoBipartito")
+        buf.append("------------------------")
+        buf.append("L1: %s" % self.l1)
+        buf.append("L2: %s" % self.l2)
+        buf.append("Cruces: no se todavía!")
+        #buf.append("Cruces: %s" % self.contarCruces())
+        buf.append("------------------------")
+        return '\n'.join(buf)
 
 
 class ResolvedorConstructivo:
