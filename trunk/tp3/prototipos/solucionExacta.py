252f0a22fbe94bceace0d1a711b4843f50bd9c0f
gomox.ar <gomox.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-05-30 05:59:23 +0000

    Agregado backtracking.

---
+++ trunk/tp3/prototipos/solucionExacta.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class ResolvedorExacto:
+    def __init__(self, dib):
+        self.dibujo = dib
+
+    def resolver(self):
+        raise NotImplementedError, "No anda todavía viteh!"
