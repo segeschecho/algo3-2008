883624c68eedcbe45ad29296eeb5f494a5b8f079
gomox.ar <gomox.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-06-05 13:28:39 +0000

    Agregado algoritmo de backtracking basico.

--- tp3/prototipos/SolucionExacta.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-class ResolvedorExacto:
-    def __init__(self, dib):
-        self.dibujo = dib
-
-    def resolver(self):
-        raise NotImplementedError, "No anda todavía viteh!"
