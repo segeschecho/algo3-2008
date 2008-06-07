d3774ac70a035bdc6cd5fa3876f5e98a495fb5a9
federicoemartinez <federicoemartinez@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-06-07 19:55:26 +0000

    el dibujador puede colorear los marcados las busquedas locales tienen una funcion hallar minimo local

--- tp3/prototipos/BusquedaLocalIntercambioGreedy.py
+++ tp3/prototipos/BusquedaLocalIntercambioGreedy.py
@@ -2,7 +2,7 @@ import random
 from HeuristicaInsercionEjes import *
 import psyco
 psyco.full()
-class busquedaLocalIntercambioGreedy:
+class BusquedaLocalIntercambioGreedy:
     
     def swapValido(self,i,j,l,marcados):
         if i in marcados:
@@ -23,7 +23,17 @@ class busquedaLocalIntercambioGreedy:
             return self.swapValido(j,i,l,marcados)
     
     
-            
+    def hallarMinimoLocal(self,dibujo,marcados1,marcados2,losEjesDe):
+        crucesInicial = contadorDeCruces(dibujo.l1,dibujo.l2,losEjesDe)
+        cambio = True
+        while cambio:
+            cambio = False
+            self.mejorar(dibujo,marcados1,marcados2,losEjesDe)
+            crucesActual = contadorDeCruces(dibujo.l1,dibujo.l2,losEjesDe)
+            if crucesActual < crucesInicial:
+                crucesInicial = crucesActual
+                cambio = True
+
     def mejorar(self,dibujo,marcados1,marcados2,losEjesDe):
         #buscamos la vecindad
         vecindad = []
@@ -80,7 +90,31 @@ class busquedaLocalIntercambioGreedy:
                 dibujo.l2[pos[0]] = pos[3]
                 dibujo.l2[pos[1]] = pos[2]
             #se hace return del nuevo dibujo y True si es que hubo cambios
-            return Dibujo(dibujo.g,dibujo.l1,dibujo.l2)
-        return dibujo
+
+
+if __name__ == '__main__':
+    g =  generarGrafoBipartitoAleatorio(12, 12, 60)
+    d = generarDibujoAleatorio(g,3, 3)
+    marcados1 = d.l1[:]
+    print marcados1
+    marcados2 = d.l2[:]
+    print marcados2
+    losEjesDe = {}
+    for each in g.p1 :
+        losEjesDe[each] = []
+    for each in g.p2 :
+        losEjesDe[each] = []
+    for each in g.ejes:
+        losEjesDe[each[0]].append(each[1])
+        losEjesDe[each[1]].append(each[0])
+        
+    res=HeuristicaInsercionEjes(d).resolver()
+    blIG=BusquedaLocalIntercambioGreedy()
+    print "antes de la busqueda",res.contarCruces()
+    blIG.hallarMinimoLocal(res,marcados1,marcados2,losEjesDe)
+    print "despues de la misma", contadorDeCruces(res.l1,res.l2,losEjesDe)
+    DibujadorGrafoBipartito(res,marcados1=marcados1,marcados2=marcados2).grabar('localMediana.svg')        
+    
+
 
 
