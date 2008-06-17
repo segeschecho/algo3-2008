2d39870d00647e2be6b3a79807c276efb0cc0b95
federicoemartinez <federicoemartinez@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-06-17 03:12:11 +0000

    prototipo para limpiar grafo

--- tp3/prototipos/GeneradorGrafos.py
+++ tp3/prototipos/GeneradorGrafos.py
@@ -6,7 +6,26 @@ from GrafoBipartito import GrafoBipartito, Dibujo
 from sets import Set
 import random
 
+def generarGrafoEntradaTp(iv1,iv2,v1,v2,m):
+    p1 = range(v1)
+    p2 = range(v1,v1+v2)
+    ip1 = range(v1+v2,v1+v2+iv1)
+    ip2 = range(v1+v2+iv1,v1+v2+iv1+iv2)
+    p1 +=ip1
+    p1=Set(p1)
+    p2 +=ip2
+    p2 = Set(p2)
+    ejesPosibles = [(a,b) for a in p1 for b in p2]
+    ejes = random.sample(ejesPosibles,m)
 
+    return GrafoBipartito(p1,p2,Set(ejes))
+def generarDibujoAleatorioEntrada(iv1,iv2,v1,v2,m):
+    g = generarGrafoEntradaTp(iv1,iv2,v1,v2,m)
+    l1 = range(v1)
+    random.shuffle(l1)
+    l2 = range(v1,v1+v2)
+    random.shuffle(l2)
+    return Dibujo(g,l1,l2)
 
 def generarGrafoBipartitoAleatorio(n1=10,n2=10,m=25):
     p1 = range(n1)
@@ -34,8 +53,29 @@ def generarDibujoAleatorio(g, n1=5, n2=5):
 
     return Dibujo(g, l1, l2)
 
-
-
+def generarEntrada(iv1,iv2,v1,v2,m):
+    d=generarDibujoAleatorioEntrada(iv1,iv2,v1,v2,m)
+    out=open("test"+str(iv1)+str(iv2)+str(v1)+str(v2)+str(m)+".in",'w')
+    out.write(str(v1)+"\n")
+    for each in d.l1:
+        out.write(str(each+1)+"\n")
+    out.write(str(v2)+"\n")
+    for each in d.l2:
+        out.write(str(each+1)+"\n")
+    ejesInternos = [(x,y) for (x,y) in d.g.ejes if x in d.l1 and y in d.l2]
+    out.write(str(len(ejesInternos))+"\n")
+    for each in ejesInternos:
+        out.write(str(each[0]+1)+" "+str(each[1]+1)+"\n")
+    out.write(str(iv1)+"\n")
+    for each in range(v1+v2,v1+v2+iv1):
+        out.write(str(each+1)+"\n")
+    out.write(str(iv2)+"\n")
+    for each in range(v1+v2+iv1, v1+v2+iv1+iv2):
+        out.write(str(each+1)+"\n")
+    ejesNuevos = [(x,y) for (x,y) in d.g.ejes if (x,y) not in ejesInternos]
+    out.write(str(len(ejesNuevos))+"\n")
+    for (x,y) in ejesNuevos:
+        out.write(str(x+1)+" " +str(y+1)+"\n")
 
 def test_generadores():
     g = generarGrafoBipartitoAleatorio(n1=10, n2=10, m=25)
@@ -46,3 +86,5 @@ def test_generadores():
 
 if __name__ == '__main__':
     test_generadores()
+    generarEntrada(4,5,7,2,20)
+    
