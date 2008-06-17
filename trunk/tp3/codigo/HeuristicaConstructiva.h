a55a9d60747d9afd810f82922623aeb37d91b98b
gomox.ar <gomox.ar@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-06-17 21:06:35 +0000

    Agregada version de Heuristica Constructiva (no compila por problemas en conteo de cruces).

---
+++ trunk/tp3/codigo/HeuristicaConstructiva.h
@@ -0,0 +1,43 @@
+#ifndef _Heuristica_Constructiva_h
+#define _Heuristica_Constructiva_h
+
+#include <vector>
+#include <list>
+#include "GrafoBipartito.h"
+#include "Dibujo.h"
+#include "conteoDeCruces.cpp"
+
+using namespace std;
+
+class HeuristicaConstructiva {
+
+public:
+    HeuristicaConstructiva(Dibujo& original);
+    Dibujo construirSolucion(float alfa=1.0, bool randomPos=false);
+
+private:
+    Dibujo* d;
+    list<nodo> fijo1, fijo2, movil1, movil2;
+    float alfa;
+    unsigned cruces;
+    bool randomPos;
+    vector< list<nodo> > adyParcial;
+    vector< unsigned > gradoParcial;
+    vector< unsigned > posiciones;
+    vector< bool > esMovil;
+
+
+    void inicializar();
+
+    nodo tomarSiguiente(list<nodo>& moviles);
+    nodo tomarSiguiente1();
+    nodo tomarSiguiente2();
+    void ordenarPorGradoParcial(list<nodo>& moviles);
+
+    void insertar(nodo, list<nodo>& fijos, list<nodo>& otrosFijos);
+    void insertarEn1(nodo);
+    void insertarEn2(nodo);
+
+};
+
+#endif
