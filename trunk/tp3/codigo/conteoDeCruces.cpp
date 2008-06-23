7297be005bd530a6e7942e58a6be1a9581cb68ee
federicoemartinez <federicoemartinez@bfd18afd-6e49-0410-abef-09437ef2666c>
2008-06-23 14:39:33 +0000

    arreglado bug de tomar max de una lista vacia

--- trunk/tp3/codigo/conteoDeCruces.cpp
+++ trunk/tp3/codigo/conteoDeCruces.cpp
@@ -4,11 +4,13 @@
 #define print(a) cout<<a<<endl;
 
 void armarIndices(const vector<nodo>& nodos, vector<unsigned int>& indicesPi) {
-    nodo tamanio = maxElem(nodos);
-    indicesPi = vector<unsigned int> (tamanio + 1);
-    for (unsigned int i = 0; i < nodos.size(); i++) {
-        indicesPi[nodos[i]] = i;
-    }
+	if(nodos.size() > 0){
+        nodo tamanio = maxElem(nodos);
+        indicesPi = vector<unsigned int> (tamanio + 1);
+        for (unsigned int i = 0; i < nodos.size(); i++) {
+            indicesPi[nodos[i]] = i;
+        }
+	}
 }
 
 nodo maxElem(const vector<nodo>& p) {
